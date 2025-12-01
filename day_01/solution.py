d67632a463d93ef4e1bb72ff60c761f06417703f
DominikWolek <dominik.artur.wolek@gmail.com>
2025-12-01 06:07:16 +0100

    Add day 01 solution

--- day_01/solution.py
+++ day_01/solution.py
@@ -4,18 +4,37 @@ import sys
 
 def part_1(input):
     result = 0
+    start = 50
+    for (direction, num) in input:
+        if direction == 'L':
+            start -= num
+        elif direction == 'R':
+            start += num
+        start = start % 100
+        if start == 0:
+            result += 1
     return result
 
 
 def part_2(input):
     result = 0
+    start = 50
+    for (direction, num) in input:
+        for i in range (0, num):
+            if direction == 'L':
+                start -= 1
+            elif direction == 'R':
+                start += 1
+            start = start % 100
+            if start == 0:
+                result += 1
     return result
 
 
 def get_input():
     input_path = "./" + sys.argv[1]
     file = open(input_path)
-    result = [line for line in file]
+    result = [(line[0], int(line[1:])) for line in file]
     return result
 
 
