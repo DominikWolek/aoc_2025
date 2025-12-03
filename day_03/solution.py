9f30e545f944a76c8285b06ae59013ebe6c07b69
DominikWolek <dominik.artur.wolek@gmail.com>
2025-12-03 06:34:57 +0100

    Add day 03 solution

--- day_03/solution.py
+++ day_03/solution.py
@@ -3,19 +3,27 @@ import sys
 
 
 def part_1(input):
-    result = 0
-    return result
+    return sum([find_largest_num(bank, 2) for bank in input])
 
 
 def part_2(input):
+    return sum([find_largest_num(bank, 12) for bank in input])
+
+
+def find_largest_num(digits, digits_cnt):
     result = 0
+    for i in range(1, digits_cnt + 1):
+        num = max(digits[: len(digits) - digits_cnt + i])
+        index = digits.index(num)
+        bank = digits[index + 1:]
+        result += num * pow(10, digits_cnt - i)
     return result
 
 
 def get_input():
     input_path = "./" + sys.argv[1]
     file = open(input_path)
-    result = [line for line in file]
+    result = [[int(i) for i in line.strip()] for line in file]
     return result
 
 
