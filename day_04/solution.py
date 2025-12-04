00c07647078938e688668602739481b0214aedd2
DominikWolek <dominik.artur.wolek@gmail.com>
2025-12-04 06:37:35 +0100

    Add day 04 solution

--- day_04/solution.py
+++ day_04/solution.py
@@ -1,14 +1,47 @@
 #!/usr/bin/env python
 import sys
+import itertools
 
 
 def part_1(input):
     result = 0
+    for y in range(len(input)):
+        for x in range(len(input[y])):
+            if accessable(input, x, y):
+                result += 1
     return result
 
 
 def part_2(input):
     result = 0
+    while (True):
+        cnt = 0
+        new_input = [list(line) for line in input]
+        for y in range(len(input)):
+            for x in range(len(input[y])):
+                if accessable(input, x, y):
+                    new_input[y][x] = 'x'
+                    cnt += 1
+                    result += 1
+        input = new_input
+        if cnt == 0:
+            return result
+
+
+def accessable(input, x, y):
+    return input[y][x] == '@' and paper_neighbours(input, x, y) < 4
+
+
+dirs = [(-1, -1), (0, -1), (-1, 0), (1, -1), (-1, 1), (1, 1), (1, 0), (0, 1)]
+
+
+def paper_neighbours(input, x, y):
+    result = 0
+    for (x_dir, y_dir) in dirs:
+        (new_x, new_y) = (x + x_dir, y + y_dir)
+        if new_y >= 0 and new_y < len(input) and new_x >= 0 and new_x < len(input[new_y]) and input[new_y][new_x] == '@':
+            result += 1
+
     return result
 
 
