eb2d7e377cded334175389355f161340a4ffe61c
DominikWolek <dominik.artur.wolek@gmail.com>
2025-12-05 06:32:45 +0100

    Add day 05 solution

--- day_05/solution.py
+++ day_05/solution.py
@@ -2,23 +2,44 @@
 import sys
 
 
-def part_1(input):
-    result = 0
-    return result
+def part_1(ranges, ids):
+    return len(list(filter(lambda id: any([does_fit(id, r) for r in ranges]), ids)))
 
 
-def part_2(input):
-    result = 0
-    return result
+def does_fit(id, range):
+    (left, right) = range
+    return id >= left and id <= right
+
+
+def part_2(ranges):
+    ranges.sort()
+    final = [ranges[0]]
+
+    for (left, right) in ranges[1:]:
+        (last_left, last_right) = final[-1]
+        if (left > last_right):
+            final.append((left, right))
+        else:
+            final[-1] = (min(left, last_left), max(right, last_right))
+
+    return sum(map(lambda range: range[1] - range[0] + 1, final))
 
 
 def get_input():
     input_path = "./" + sys.argv[1]
     file = open(input_path)
-    result = [line for line in file]
-    return result
+    ranges = []
+    ids = []
+    for line in file:
+        if line != "\n":
+            if line.find('-') != -1:
+                splitted = line.split('-')
+                ranges.append((int(splitted[0]), int(splitted[1])))
+            else:
+                ids.append(int(line))
+    return (ranges, ids)
 
 
-input = get_input()
-print("Part 1:", part_1(input))
-print("Part 2:", part_2(input))
+(ranges, ids) = get_input()
+print("Part 1:", part_1(ranges, ids))
+print("Part 2:", part_2(ranges))
