ef67480b1d7d4c3405bcc961813ee8fefd805ae8
DominikWolek <dominik.artur.wolek@gmail.com>
2025-12-08 06:58:42 +0100

    Cleanup day_07

--- day_07/solution.py
+++ day_07/solution.py
@@ -5,18 +5,21 @@ import sys
 def part_1(input):
     result = 0
 
-    input[1][input[0].index('S')] = '|'
+    curr = set()
+    curr.add(input[0].index('S'))
+    next = set()
 
-    for i in range(2, len(input) - 1):
-        beam = 0
-        for _ in range(input[i - 1].count('|')):
-            beam = input[i - 1][beam+1:].index('|') + beam + 1
-            if input[i][beam] == '.':
-                input[i][beam] = '|'
-            elif input[i][beam] == '^':
-                input[i][beam + 1] = '|'
-                input[i][beam - 1] = '|'
+    for i in range(1, len(input)):
+        for beam in curr:
+            if input[i][beam] == '^':
+                next.add(beam - 1)
+                next.add(beam + 1)
                 result += 1
+            else:
+                next.add(beam)
+
+        curr = next
+        next = set()
 
     return result
 
@@ -24,27 +27,21 @@ def part_1(input):
 def part_2(input):
     timelines = {}
 
-    start = input[0].index('S')
-    input[1][start] = '|'
-    timelines[(1, start)] = 1
+    curr = {input[0].index('S'): 1}
+    next = {}
 
-    for i in range(2, len(input) - 1):
-        beam = 0
-        for _ in range(input[i - 1].count('|')):
-            beam = input[i - 1][beam+1:].index('|') + beam + 1
+    for i in range(1, len(input)):
+        for beam in curr.keys():
             if input[i][beam] == '^':
-                timelines[(i, beam + 1)] = timelines.get((i,
-                                                          beam + 1), 0) + timelines[(i - 1, beam)]
-                timelines[(i, beam - 1)] = timelines.get((i,
-                                                          beam - 1), 0) + timelines[(i - 1, beam)]
-                input[i][beam + 1] = '|'
-                input[i][beam - 1] = '|'
+                next[beam + 1] = next.get(beam + 1, 0) + curr[beam]
+                next[beam - 1] = next.get(beam - 1, 0) + curr[beam]
             else:
-                input[i][beam] = '|'
-                timelines[(i, beam)] = timelines.get((i,
-                                                      beam), 0) + timelines[(i - 1, beam)]
+                next[beam] = next.get(beam, 0) + curr[beam]
 
-    return sum(timelines.get((len(input) - 2, x), 0) for x in range(len(input[0])))
+        curr = next
+        next = {}
+
+    return sum(curr.values())
 
 
 def get_input():
@@ -55,5 +52,5 @@ def get_input():
 
 
 input = get_input()
-print("Part 1:", part_1(list([list(line) for line in input])))
+print("Part 1:", part_1(input))
 print("Part 2:", part_2(list(input)))
